509745d6c320a8aed53e6bb99e90052f61f77d7b
Alessandro Paghera <apaghera@2-G-7.42heilbronn.de>
2024-06-12 12:46:58 +0200

    added some changes on DB script

--- srcs/services/mariaDB/expand.sh
+++ srcs/services/mariaDB/expand.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-# Generate init.sql content
-#
-cat << EOF > init.sql
-CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
 
-mysql_upgrade
-mysql_install_db
-mysqld
+# Color codes
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Prepare a SQL file with initialization commands using echo commands
+echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" > /tmp/init.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /tmp/init.sql
+echo "GRANT ALL PRIVILEGES ON \`${MYSQL_NAME}\`.* TO '${MYSQL_USER}'@'%';" >> /tmp/init.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/init.sql
+
+echo -e "${GREEN}Init SQL file created${NC}"
+
+# Start MariaDB in the background temporarily for initialization
+mysqld_safe --skip-networking &
+
+# Wait for MariaDB to be ready
+while ! mysqladmin ping --silent; do
+    echo -e "${YELLOW}Waiting for mysql...${NC}"
+    sleep 1
+done
+
+mysql < /tmp/init.sql
+mysqladmin shutdown
+
+echo -e "${GREEN}MariaDB Ready${NC}"
+
+mysqld_safe
